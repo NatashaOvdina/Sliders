e69f2975f2d56ba258993a67a46e77f5ae70000b
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-17 19:46:04 +0300

    Changed colors. Wrote Outlets for objects (from a screen) in code.

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
