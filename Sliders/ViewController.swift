9dbb47ae7f1032a6a2343fd11889616840c57437
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-19 17:58:25 +0300

    Put sliders'values into label's value

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var mainView: UIView!
     
@@ -22,8 +22,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 10
+        
     }
 
+    @IBAction func redSliderAction() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        mainView.alpha = CGFloat(redSlider.value)
+        
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+        
+    }
+    
 
 }
+
+
 
