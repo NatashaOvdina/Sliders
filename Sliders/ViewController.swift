7a384c4188edeb01a0df2626f9940060557502e9
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-19 19:38:21 +0300

    Make connection b/w sliders' values and view's background. Finished the code.

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
     @IBOutlet var mainView: UIView!
     
     @IBOutlet var greenValue: UILabel!
@@ -19,30 +20,45 @@ final class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 10
-        
+        makeColoredBackground()
     }
-
+    
+    // MARK: - IB Actions
     @IBAction func redSliderAction() {
         redValue.text = String(format: "%.2f", redSlider.value)
-        mainView.alpha = CGFloat(redSlider.value)
-        
-        
+        makeColoredBackground()
     }
     
     @IBAction func greenSliderAction() {
         greenValue.text = String(format: "%.2f", greenSlider.value)
-        
+        makeColoredBackground()
     }
     
     @IBAction func blueSliderAction() {
         blueValue.text = String(format: "%.2f", blueSlider.value)
-        
+        makeColoredBackground()
     }
     
-
+    // MARK: - Private Methods
+    private func makeColoredBackground() {
+        let backgroundColored = UIColor(
+            red: CGFloat(
+                redSlider.value
+            ),
+            green: CGFloat(
+                greenSlider.value
+            ),
+            blue: CGFloat(
+                blueSlider.value
+            ),
+            alpha: 1
+        )
+        mainView.backgroundColor = backgroundColored
+    }
 }
 
 
