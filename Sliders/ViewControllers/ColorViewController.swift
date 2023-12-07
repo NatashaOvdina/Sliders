ba88ebc0e25c0545a5e28259b8a93f92d8a578fe
NatashaOvdina <natashaovdina@yahoo.com>
2023-12-07 17:42:10 +0300

    Changed the name of SettingsVC. Did work in Storyboard (add VC, buttons, constraints, segue).

---
+++ Sliders/ViewControllers/ColorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ColorViewController.swift
+//  Sliders
+//
+//  Created by Natalia Ovdina on 07.12.2023.
+//
+
+import UIKit
+
+final class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+
+}
